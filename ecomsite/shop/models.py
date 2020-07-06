8361c179d0291a52d773b37ba26d72fd105f92d1
aleksgan <aleksandrgaranin89@gmail.com>
2020-07-06 15:38:42 -0700

    created index.html and connecting db

--- ecomsite/shop/models.py
+++ ecomsite/shop/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Products(models.Model):
     def __str__(self):
-        return self.name
+        return self.title
     
     title = models.CharField(max_length=200)
     price = models.FloatField()
