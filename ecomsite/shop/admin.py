0875a1d9f9cd286903e5040d0c5c99582c98447c
aleksgan <aleksandrgaranin89@gmail.com>
2020-07-10 08:30:48 -0700

    admin panel customisation

--- ecomsite/shop/admin.py
+++ ecomsite/shop/admin.py
@@ -2,5 +2,20 @@ from django.contrib import admin
 from .models import Products, Order
 # Register your models here.
 
-admin.site.register(Products)
+admin.site.site_header = "Decor Online"
+admin.site.site_title = "Decor Online"
+admin.site.index_title = "Manage Decor Online"
+
+class ProductAdmin(admin.ModelAdmin):
+
+    def change_category_to_default(self,request,queryset):
+        queryset.update(category="default")
+
+    change_category_to_default.short_description = "Default Category"
+    list_display = ('title','price','discount_price', 'category', 'description',)
+    search_fields = ('title','description','category',)
+    actions = ('change_category_to_default',)
+
+
+admin.site.register(Products, ProductAdmin)
 admin.site.register(Order)
